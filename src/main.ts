144855046eaa027845c4d2ba3d6f6c0402c53055
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2026-08-20 18:27:44 -0400

    stop flashing

--- src/main.ts
+++ src/main.ts
@@ -72,6 +72,6 @@ app.config.errorHandler = (err, _instance, info) => {
 // visitor's catalog doesn't pop in late. Not waiting on router.isReady()
 // too: the route-dependent boot work now lives in a data loader attached
 // to the road/explore pages (loaders/appBoot.ts), not in App.vue.
-void isCacheReady().then(() => {
+void Promise.all([isCacheReady(), router.isReady()]).then(() => {
   app.mount("#app");
 });
