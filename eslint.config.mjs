a8b0802880a8b5eadf04f1693c0a94320ae1927b
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2026-09-22 18:12:54 -0400

    deprecated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
+import { defineConfig } from "eslint/config";
+
 import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 import prettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: [
       "dist/",
